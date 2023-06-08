2d7e189856f067269ff8810d0e9221cb612a14d8
Aryan <aryan34.model@gmail.com>
2023-06-08 23:41:36 +0530

    cors = server url

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app=express();
 const PORT=8000;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({extended: true}))
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-// app.use(cors({ credentials: true, origin: 'https://landingpage77.netlify.app/' }));
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://landingpage77.netlify.app' }));
 app.listen(PORT,()=>{console.log("server is on :" + PORT)});
 
 connecting();
